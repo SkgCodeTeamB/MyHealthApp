d0162810eb2453b6d102d1bc4d99c696abc917a3
christos-kouros <cekouros@csd.auth.gr>
2021-09-29 17:18:29 +0300

    commented out some code

--- Server/index.js
+++ Server/index.js
@@ -6,14 +6,14 @@ import dotenv from "dotenv";
 
 //Import Routes
 import userRoutes from "./routes/user.js";
-import appointmentRoutes from "./routes/appointment.js";
-import doctorRoutes from "./routes/doctor.js";
-import fieldRoutes from "./routes/field.js";
-import diagnosesRoutes from "./routes/diagnoses.js";
+// import appointmentRoutes from "./routes/appointment.js";
+// import doctorRoutes from "./routes/doctor.js";
+// import fieldRoutes from "./routes/field.js";
+//import diagnosesRoutes from "./routes/diagnoses.js";
 import hospitalsRoute from "./routes/hospitals.js";
 import vaccinesRoute from "./routes/vaccines.js";
-import prescriptionsRoute from "./routes/prescriptions.js";
-import vaccinationRoute from "./routes/vaccinations.js";
+//import prescriptionsRoute from "./routes/prescriptions.js";
+//import vaccinationRoute from "./routes/vaccinations.js";
 
 const app = express();
 dotenv.config();
@@ -23,14 +23,14 @@ app.use(bodyParser.urlencoded());
 app.use(cors());
 
 app.use("/user", userRoutes);
-app.use("/appointment", appointmentRoutes);
-app.use("/doctor", doctorRoutes);
-app.use("/field", fieldRoutes);
-app.use("/diagnose", diagnosesRoutes);
+// app.use("/appointment", appointmentRoutes);
+// app.use("/doctor", doctorRoutes);
+// app.use("/field", fieldRoutes);
+//app.use("/diagnose", diagnosesRoutes);
 app.use("/hospital", hospitalsRoute);
 app.use("/vaccine", vaccinesRoute);
-app.use("/prescription", prescriptionsRoute);
-app.use("/vaccination", vaccinationRoute);
+//app.use("/prescription", prescriptionsRoute);
+//app.use("/vaccination", vaccinationRoute);
 
 const PORT = process.env.PORT;
 
