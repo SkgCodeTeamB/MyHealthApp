5f79b6bb29d46dd973128c66e6a60201a02c011f
christos-kouros <cekouros@csd.auth.gr>
2021-10-04 19:16:03 +0300

    Update and delete is now supported

--- Server/controllers/user.js
+++ Server/controllers/user.js
@@ -2,8 +2,7 @@ import UserSchema from "../models/user.js";
 
 export const getUsers = async (req, res) => {
     try {
-        const users = await UserSchema.find();
-
+        const users = await UserSchema.find().populate('familydoctor');
         res.status(200).json(users);
     } catch (err) {
         res.status(404).json({message: err.message});
@@ -27,7 +26,7 @@ export const addUser = async (req, res) => {
 //send all the information of given user' amka (req.body.amka)
 export const getInfo = async (req, res) => {
     try {
-        const users = await UserSchema.find({amka: req.params.amka});
+        const users = await UserSchema.find({amka: req.params.amka}).populate('familydoctor');
 
         res.status(200).json(users);
     } catch (err) {
@@ -48,7 +47,7 @@ export const deleteUser = async (req, res) => {
 
 export const updateUser = async (req, res) => {
     try {
-        const updateUser = await UserSchema.updateOne({  amka: req.body.amka }, { $set: { name: req.body.name, surname: req.body.surname, email: req.body.email, phone: req.body.phone, birthday: req.body.birthday, bloodtype: req.body.bloodtype , amka: req.body.amka, familydoctor: req.body.familydoctor, address: req.body.address, city: req.body.city, postacode: req.body.postalcode } });
+        const updateUser = await UserSchema.updateOne({  amka: req.body.amka }, { $set: { name: req.body.name, surname: req.body.surname, email: req.body.email, phone: req.body.phone, birthday: req.body.birthday, bloodtype: req.body.bloodtype , amka: req.body.amka,  address: req.body.address, city: req.body.city, postacode: req.body.postalcode } });
 
         res.status(200).json(updateUser);
     } catch (err) {
