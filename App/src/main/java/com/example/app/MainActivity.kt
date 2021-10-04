8003c543462289321d3c3a68c1d635c3faf855e2
dai18088 <dai18088@uom.edu.gr>
2021-10-05 02:02:53 +0300

    commit_2

--- App/src/main/java/com/example/app/MainActivity.kt
+++ App/src/main/java/com/example/app/MainActivity.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,12 +17,28 @@ class MainActivity : AppCompatActivity() {
         val btn_Register : Button = findViewById(R.id.btn_Register)
         val btn_Login : Button = findViewById(R.id.btn_Login)
 
+        val editText_AMKA : EditText = findViewById(R.id.editText_AMKA)
 
         btn_Register.setOnClickListener {
             // your code to perform when the user clicks on the button
             val intentRegister = Intent (this, Register::class.java)
             startActivity(intentRegister)
         }
+
+        btn_Login.setOnClickListener{
+
+            if(editText_AMKA.text.toString().trim().isEmpty()) {
+                editText_AMKA.error = "Required"
+                //Toast.makeText(applicationContext, "AMKA is Required for Login", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                // After successful register you will move on new activity
+                val intentLogin = Intent(this, HomePage::class.java)
+                startActivity(intentLogin)
+                Toast.makeText(this@MainActivity, "Your Login is successful.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
 
     }
 }
