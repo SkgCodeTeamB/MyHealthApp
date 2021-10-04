500443a97991b78ac9a14a051991249ed84f10bf
christos-kouros <cekouros@csd.auth.gr>
2021-10-04 18:48:57 +0300

    Update and delete is now supported

--- Server/controllers/user.js
+++ Server/controllers/user.js
@@ -1,45 +1,57 @@
 import UserSchema from "../models/user.js";
 
 export const getUsers = async (req, res) => {
-  try {
-    const users = await UserSchema.find().populate('familydoctor');
+    try {
+        const users = await UserSchema.find();
 
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(404).json({ message: error.message });
-  }
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(404).json({message: err.message});
+    }
 };
 
 export const addUser = async (req, res) => {
-  try {
-    const user = new UserSchema({
-      name: req.body.name,
-      surname: req.body.surname,
-      email: req.body.email,
-      phone: req.body.phone,
-      birthday: req.body.birthday,
-      bloodtype: req.body.bloodtype,
-      amka: req.body.amka,
-      familydoctor: req.body.familydoctor,
-      address: req.body.address,
-      city: req.body.city,
-      postalcode: req.body.postalcode
-    });
+    try {
+        const user = new UserSchema({
+            name: req.body.name,
+            amka: req.body.amka
+        });
 
-    const savedUser = await user.save();
-    res.status(200).json(savedUser);
-  } catch (err) {
-    res.status(404).json({ message: error.message });
-  }
+        const savedUser = await user.save();
+        res.status(200).json(savedUser);
+    } catch (err) {
+        res.status(404).json({message: err.message});
+    }
 };
 
 //send all the information of given user' amka (req.body.amka)
 export const getInfo = async (req, res) => {
-  try {
-    const users = await UserSchema.find({ amka: req.params.amka }).populate('familydoctor');
+    try {
+        const users = await UserSchema.find({amka: req.params.amka});
 
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(404).json({ message: error.message });
-  }
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(404).json({message: err.message});
+    }
+};
+
+
+export const deleteUser = async (req, res) => {
+    try {
+        const deleteUser = await UserSchema.deleteOne({ _id: req.params.id });
+
+        res.status(200).json(deleteUser);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
+
+export const updateUser = async (req, res) => {
+    try {
+        const updateUser = await UserSchema.updateOne({  amka: req.body.amka }, { $set: { name: req.body.name, surname: req.body.surname, email: req.body.email, phone: req.body.phone, birthday: req.body.birthday, bloodtype: req.body.bloodtype , amka: req.body.amka, familydoctor: req.body.familydoctor, address: req.body.address, city: req.body.city, postacode: req.body.postalcode } });
+
+        res.status(200).json(updateUser);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
 };
--- Server/routes/user.js
+++ Server/routes/user.js
@@ -6,14 +6,20 @@ import { getInfo } from "../controllers/user.js";
 import { getVaccinations } from "../controllers/vaccination.js";
 import { getDiagnoses } from "../controllers/diagnoses.js";
 import { getPrescriptions } from "../controllers/prescriptions.js";
+import {updateUser} from "../controllers/user.js";
+import {deleteUser} from "../controllers/user.js";
+
 
 const router = express.Router();
 
 router.get("/", getUsers);
 router.post("/add", addUser);
+router.patch("/updateUser", updateUser);
 router.get("/vaccinations", getVaccinations);
 router.get("/diagnoses", getDiagnoses);
 router.get("/prescriptions", getPrescriptions);
 router.get("/:amka", getInfo);
+router.delete("/delete/:id", deleteUser);
+
 
 export default router;
