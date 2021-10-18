e925d2e3428bcfa716283a70f93027fed77fba02
dai18088 <dai18088@uom.edu.gr>
2021-10-18 22:46:50 +0300

    Correct Login and Register

--- App/src/main/java/com/example/app/api/LoginRespnose.kt
+++ App/src/main/java/com/example/app/api/LoginRespnose.kt
@@ -2,5 +2,17 @@ package com.example.app.api
 
 
 data class LoginResponse(
-    val amka: String
+    val token: String,
+    val _id: String,
+    val name: String,
+    val surname: String,
+    val email: String,
+    val phone: String,
+    val birthday: String,
+    val bloodtype: String,
+    val amka: String,
+    val familydoctor: String,
+    val address: String,
+    val city: String,
+    val postalcode: String
 )
