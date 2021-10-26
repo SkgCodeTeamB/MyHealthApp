60c832790df8d9cca11baeeb74f226c62c1aa0ed
dai18088 <dai18088@uom.edu.gr>
2021-10-26 14:33:19 +0300

    background color

--- App/src/main/java/com/example/app/ApiInterface.kt
+++ App/src/main/java/com/example/app/ApiInterface.kt
@@ -150,8 +150,8 @@ interface ApiInterface {
 
     companion object {
         //var BASE_URL = "http://192.168.1.5:5000/"
-        var BASE_URL = "http://192.168.1.3:5000/"
-        //var BASE_URL = "http://192.168.0.199:5000/"
+        //var BASE_URL = "http://192.168.1.3:5000/"
+        var BASE_URL = "http://192.168.0.199:5000/"
 
         fun create(): ApiInterface {
 
