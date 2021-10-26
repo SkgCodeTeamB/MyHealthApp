66eb75b277f60c70c7c1bbae9f76046ddc9f5f5e
christos-kouros <cekouros@csd.auth.gr>
2021-10-26 14:11:53 +0300

    Changed Appointments layout

---
+++ App/src/main/java/com/example/app/Models/Appointments/AppointmentData.kt
@@ -0,0 +1,3 @@
+package com.example.app.Models.Appointments
+
+data class AppointmentData(var doctor: String, var date: String, var time: String)
--- App/src/main/java/com/example/app/Models/Appointments/RecyclerData.kt
+++
@@ -1,3 +0,0 @@
-package com.example.app.Models.Appointments
-
-data class RecyclerData(var speciallity: String, var doctor: String, var date: String, var time: String)
